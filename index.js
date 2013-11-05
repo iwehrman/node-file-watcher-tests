1ef3d433990b8d7ab918a7f2c594955907ffa174
Ian Wehrman <wehrman@adobe.com>
2013-11-04 17:14:42 -0800

    blah

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ if (process.platform === "darwin") {
 }
 
 var WIDTH = 2,
-    DEPTH = 10;
+    DEPTH = 3,
+    MAX_WAIT_TIME = 5000;
 
-var changedPaths = {};
+var callbacksReceived = 0,
+    callbacksLost = 0,
+    changedPaths = {};
 
 function range(count, getValue) {
     var result = [],
@@ -43,21 +46,36 @@ function randomBitVector(count) {
 }
 
 function randomPath(depth) {
-    return randomBitVector.join("/");
+    return randomBitVector(depth).join("/");
 }
 
 function triggerChange(path) {
-    fs.appendFile(".");
+    return fs.appendFileAsync(path + "/" + "a", ".");
 }
 
-function watcherTest() {
+function oneWatcherTest(dir) {
     var depth = Math.floor(Math.random() * DEPTH) + 1,
-        path = randomPath(DEPTH);
+        path = dir + "/" + randomPath(DEPTH);
     
+    var changePromise = new Promise(function (resolve, reject) {
+        var timer = setTimeout(function () {
+            console.log("Lost change: ", path, callbacksLost++);
+            reject(path);
+            delete changedPaths[path];
+        }, MAX_WAIT_TIME);
+        
+        changedPaths[path] = {
+            resolve: resolve,
+            timer: timer
+        };
+    });
     
-    triggerChange(path);
+    return Promise.join(triggerChange(path), changePromise);
 }
 
+function continuousWatcherTest(dir) {
+    oneWatcherTest(dir).lastly(continuousWatcherTest.bind(undefined, dir));
+}
 
 function createDirectories(path, width, depth) {
     if (--depth < 0) {
@@ -91,9 +109,12 @@ function createDirectories(path, width, depth) {
 function getAllDirectories(dirname) {
     return fs.readdirAsync(dirname)
         .then(function (names) {
-            var paths = names.map(function (name) {
+            var paths = names.filter(function (name) {
+                return !(name.length > 0 && name[0] === ".");
+            }).map(function (name) {
                 return [dirname, name].join("/");
             });
+            
             var statPromises = paths.map(function (path) {
                 return fs.statAsync(path);
             });
@@ -132,13 +153,19 @@ function getAllDirectories(dirname) {
         });
 }
 
-function watchPath(path, force, callback) {
-    var watcher;
-    
-    if (typeof force === "function") {
-        callback = force;
-        force = false;
+function watchedFileCallback(dir) {
+    if (changedPaths.hasOwnProperty(dir)) {
+        clearTimeout(changedPaths[dir].timer);
+        changedPaths[dir].resolve();
+        delete changedPaths[dir];
+        console.log("Received expected change: ", dir, callbacksReceived++);
+    } else {
+        console.log("Received unexpected change: ", dir);
     }
+}
+
+function watchPath(path, force) {
+    var watcher;
     
     if (path[path.length - 1] === "/") {
         path = path.substring(0, path.length - 1);
@@ -147,9 +174,9 @@ function watchPath(path, force, callback) {
     if (fsevents && !force) {
         console.log("Recursively watching: ", path);
         watcher = fsevents(path);
-        watcher.on("change", function (path, info) {
-            console.log("fsevents: ", path, info);
-            callback(path);
+        watcher.on("change", function (path) {
+            var dir = path.substring(0, path.lastIndexOf("/"));
+            watchedFileCallback(dir);
         });
         return Promise.fulfilled();
     } else {
@@ -157,22 +184,9 @@ function watchPath(path, force, callback) {
             .map(function (dirname) {
                 console.log("Directly watching: ", dirname);
                 watcher = fs.watch(dirname);
-                watcher.on("change", function (path) {
-                    console.log("fs.watch: ", path);
-                    callback(path);
-                });
-                watcher.on("error", function (err) {
-                    console.log(err);
-                });
+                watcher.on("change", watchedFileCallback.bind(undefined, dirname));
+                watcher.on("error", console.log.bind(console));
             });
-    }
-}
-
-function watchedFileCallback(path) {
-    if (changedPaths.hasOwnProperty(path)) {
-        console.log("Received expected change for: ", path);
-    } else {
-        console.log("Received unexpected change for: ", path);
     }
 }
 
@@ -183,5 +197,9 @@ if (!path) {
     process.exit(1);
 }
 
-createDirectories(path, 2, 10)
-    .then(watchPath.bind(undefined, path, watchedFileCallback));
+fs.realpathAsync(path).then(function (realpath) {
+    createDirectories(realpath, WIDTH, DEPTH)
+        .then(watchPath.bind(undefined, realpath, true))
+//        .then(oneWatcherTest.bind(undefined, realpath));
+        .then(continuousWatcherTest.bind(undefined, realpath));
+});
